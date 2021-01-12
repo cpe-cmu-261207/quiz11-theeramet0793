253efe1d2d720bbc435afc9f9f429e0358a285ac
theeramet0793 <theerametmetha@gmail.com>
2021-01-12 13:53:32 +0700

    test

--- app_template.js
+++ app_template.js
@@ -1,1 +1,5 @@
 //your code here!
+let new_id = document.createElement("compute-result")
+let x = document.getElementById("input-number").innerHTML 
+new_id = Number(620610793)-Number(x)
+console.log(new_id)
